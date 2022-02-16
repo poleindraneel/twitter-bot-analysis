250a03879390c37084a90f70098118ebb0dd6aa3
Indraneel Pole <pole.indraneel@gmail.com>
2022-02-16 16:39:26 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    consumer_key: 'nvYQS30kGYsD9tMyHUsexzhMd',
-    consumer_secret: 'cBeY6qSkRGXKAvtqSAWlhbKUIZ1PCukiLOoQ5u4b6Opm1Mw3R9',
-    access_token_key: '67331183-sW6YufKbuj2HiOmtYOVvg8rn4Ob9FtDZwweK015Uj',
-    access_token_secret: '9cwU3pkBc23ZMMVr0auMhNNOQeMZFUq8GQtnsfob6x45s'
+    consumer_key: '',
+    consumer_secret: '',
+    access_token_key: '-',
+    access_token_secret: ''
   }
